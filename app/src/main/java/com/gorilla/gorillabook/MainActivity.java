330ef0fa048530ba73ce9c75041adbd220a4cfff
Marcos Sandoval Calvo <marcos.sandoval@avantica.net>
2019-03-16 18:22:58 -0600

    Commiting splash screen

--- app/src/main/java/com/gorilla/gorillabook/MainActivity.java
+++ app/src/main/java/com/gorilla/gorillabook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gorilla.gorillabook;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content, new SplashScreen());
+        transaction.commit();
+
     }
 }
